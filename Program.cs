c46ca58e59862880366833d7e68237a604ab625c
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-08 22:35:20 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Program_latihan_queue_enqueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        static int[] Antrian;
+        static int Tail, Head, tailidx = -1, headidx = -1;
+
+        void createAntrian()
+        {
+        Antrian = new int[8];
+        Tail = 0; Head = 0;
+        }
+
+        void enQUEUE(int elment)
+        {
+        Antrian[tailidx + 1] = elment;
+        tailidx++;
+        headidx = 0;
+        Tail = element; Head = Antrian[headidx];
+        }
+    }
+}
